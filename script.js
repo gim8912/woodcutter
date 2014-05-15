8c33022a00d3e3c3824304af5e3a7be2488bb5f0
gim8912 <gim8912@gmail.com>
2014-05-15 23:00:40 +0900

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ $(document).ready(function(){
 		}
 	}
 	var gradient = function(number){
-		$('#axe').css({"-webkit-transform": "rotate("+number*10+"deg)"});
+		$('.axe').css({"-webkit-transform": "rotate("+number*10+"deg)"});
 	}
 
 	var firepoint = function(){
